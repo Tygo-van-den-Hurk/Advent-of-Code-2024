7b7c5ad32ccd78fb9118c3ba5fa765cb4297ebfb
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-03 10:25:29 +0100

    Added tests for part 2 for day 3.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -48,5 +48,10 @@ mod tests {
     fn test_part1() {
         assert_eq!(part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"), 161);
     }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"), 48);
+    }
 }
 
