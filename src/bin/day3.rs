bb01435c4ff7562e13c9ba82ffd860d7838ed160
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-03 10:23:49 +0100

    Completed part 1 for day 3.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 const INPUT: &str = include_str!("../doc/day3.txt");
 
 fn main() {
@@ -15,7 +17,22 @@ fn main() {
 
 //` Part 1
 
-pub fn part1(input: &str) -> i32 { 0 }
+pub fn part1(input: &str) -> i32 { 
+    let mut total = 0;
+    let regular_expression: Regex = Regex::new(r"mul\((\d*,\d*)\)").unwrap();
+    for matching_instance in regular_expression.find_iter(&input) {
+        let mut captured_substring: &str = matching_instance.as_str();
+        println!("Captured string: {captured_substring}");
+        captured_substring = &captured_substring[4..captured_substring.len()-1];
+        println!("Substring of Captured string: {captured_substring}");
+        let split_captured_substring: Vec<&str> = captured_substring.split(",").collect();
+        let left = split_captured_substring[0].parse::<i32>().unwrap();
+        let right = split_captured_substring[1].parse::<i32>().unwrap();
+        total += left * right;
+    }
+
+    return total;
+}
 
 //` Part 2
 
