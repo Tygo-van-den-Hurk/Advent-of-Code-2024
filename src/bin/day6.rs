fc67ebd0b67f4f23294805a7e6a2baf621305e4a
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-06 15:13:59 +0100

    Created a template for day 6

---
+++ src/bin/day6.rs
@@ -0,0 +1,44 @@
+const INPUT: &str = include_str!("../doc/day6.txt");
+
+fn main() {
+    
+    /* Solving Part 1 based on the input */ {
+        let result: i32 = part1(INPUT);
+        println!("The solution to part 1 is: \"{result}\".");
+    }
+        
+    /* Solving Part 1 based on the input */ {
+        let result: i32 = part2(INPUT);
+        println!("The solution to part 2 is: \"{result}\".");
+    }
+}
+
+//` Part 1
+
+pub fn part1(input: &str) -> i32 { 0 }
+
+//` Part 2
+
+pub fn part2(input: &str) -> i32 { 0 }
+
+//` Test
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."), 41);
+    }
+}
+
