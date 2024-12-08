225f6df77818a81de66c117e90ea7e906b89e943
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-08 10:41:02 +0100

    Completed part 2 of day 8.

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -81,7 +81,73 @@ mod part1 {
 
 mod part2 {
     
-    pub fn solve(input: &str) -> usize { input.len() }
+    use std::{collections::{HashMap, HashSet}, vec};
+
+    #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    struct Point { x: usize, y: usize }
+
+    #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    struct Size { width: usize, height: usize }
+
+    pub fn solve(input: &str) -> usize { 
+
+        let map_size;
+        /* Getting the map sizes */ {
+            let height =  input.lines().collect::<Vec<_>>().len();
+            let width = input.len() / height;
+            map_size = Size { width, height };
+        }
+
+        let mut lookup_cache: HashMap<char, Vec<Point>> = HashMap::new();
+        /* Getting the antennas from the map, storing where they are and grouping them by type */ {
+            for (y, line) in input.lines().rev().enumerate() {
+                for (x, character) in line.chars().enumerate() {
+                    
+                    if character == '.' { continue; } 
+
+                    if let Some(value) = lookup_cache.get_mut(&character) {
+                        value.push( Point { x, y } );
+                        continue;
+                    }
+                    
+                    lookup_cache.insert(character, vec![ Point {x, y} ] );
+                }
+            }
+        }
+
+        let mut antinode_locations: HashSet<Point> = HashSet::new();
+        /* Calculating the amount of antinode locations onto the map */ {
+            for antenna_locations in lookup_cache.values() {
+                for antenna1 in antenna_locations {
+                    for antenna2 in antenna_locations {
+
+                        if *antenna1 == *antenna2 { continue; }
+                        
+                        antinode_locations.insert(antenna1.clone()); // an antenna is also a antinode point now
+
+                        let mut iteration = 1;
+                        loop {
+                            let x_diff: i32 = (antenna1.x as i32) - (antenna2.x as i32);
+                            let new_x: i32 = (antenna1.x as i32) + x_diff * iteration;
+                            if new_x < 0 || new_x >= map_size.width  as i32 { break; }
+
+                            let y_diff: i32 = (antenna1.y as i32) - (antenna2.y as i32);
+                            let new_y: i32  = (antenna1.y as i32) + y_diff * iteration;
+                            if new_y < 0 || new_y >= map_size.height as i32 { break; }
+                            
+                            let point = Point { x: new_x as usize, y: new_y as usize };
+                            antinode_locations.insert(point);
+
+                            iteration += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        println!("{:?}", antinode_locations);
+        return antinode_locations.len();
+    }
 
 }
 
@@ -106,6 +172,20 @@ mod tests {
 .........A..
 ............
 ............"), 14);
+    }
+
+    #[test]
+    fn test_part2_simple() {
+        assert_eq!(part2::solve("T.........
+...T......
+.T........
+..........
+..........
+..........
+..........
+..........
+..........
+.........."), 9);
     }
 
     #[test]
