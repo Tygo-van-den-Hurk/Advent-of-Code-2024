69081e8831157f71eae6b8331ecb386ba6fe1552
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-14 16:51:09 +0100

    Finished part 2 for day 14.

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -208,9 +208,200 @@ mod part1 {
 /// The part that solves the first part of the puzzle.
 mod part2 {
 
-    /// The function that solves the puzzle.
-    pub fn solve(input: &str) -> usize { input.len() }
+    use std::str::FromStr;
+    use std::ops::Add;
+    use std::num::ParseIntError;
+    use std::io::{self, Write};
 
+    /// Custom error type
+    #[derive(Debug)]
+    pub enum ParseError {
+        InvalidFormat,
+        ParseIntError(ParseIntError),
+    } impl From<ParseIntError> for ParseError {
+        fn from(err: ParseIntError) -> Self {
+            ParseError::ParseIntError(err)
+        }
+    }
+
+    /// The dimensions of the bathroom that the robots guard.
+    #[cfg(test)]
+    const BATHROOM_DIMENSIONS: Dimensions = Dimensions { width: 11, height: 7 };
+    
+    /// The dimensions of the bathroom that the robots guard.
+    #[cfg(not(test))]
+    const BATHROOM_DIMENSIONS: Dimensions = Dimensions { width: 101, height: 103 };
+
+    /// The amount of seconds to simulate.
+    // #[cfg(test)]
+    const SIMULATION_DURATION: usize = 100;
+
+    // #[cfg(not(test))]
+    // const SIMULATION_DURATION: usize = 100;
+
+    /// An abstraction for a point in 2D space.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+    pub struct Dimensions { 
+        /// The width of something
+        width: i32, 
+        /// The height of something
+        height: i32 
+    }
+    
+    //` - - - - - struct Point - - - - -
+
+    /// An abstraction for a point in 2D space.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+    pub struct Point { 
+        /// The x-coordinate of the point
+        x: i32, 
+        /// The y-coordinate of the point
+        y: i32 
+    
+    } impl Add<Velocity> for Point { type Output = Point;
+    
+        fn add(self, velocity: Velocity) -> Point {
+            Point {
+                x: self.x + velocity.x,
+                y: self.y + velocity.y,
+            }
+        }
+
+    } impl FromStr for Point { 
+        type Err = ParseError;
+        
+        fn from_str(input: &str) -> Result<Self, Self::Err> {
+            let parts: Vec<&str> = input.split(',').collect();
+            
+            if parts.len() != 2 { return Err(ParseError::InvalidFormat); }
+    
+            let x = parts[0].parse::<i32>()?;
+            let y = parts[1].parse::<i32>()?;
+    
+            Ok(Point { x, y })
+        }
+    }
+    
+    //` - - - - - struct Velocity - - - - -
+
+    /// An abstraction for a point in 2D space.
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+    pub struct Velocity { x: i32, y: i32 } impl FromStr for Velocity { 
+        type Err = ParseError;
+        
+        fn from_str(input: &str) -> Result<Self, Self::Err> {
+            let parts: Vec<&str> = input.split(',').collect();
+            
+            if parts.len() != 2 { return Err(ParseError::InvalidFormat); }
+    
+            let x = parts[0].parse::<i32>()?;
+            let y = parts[1].parse::<i32>()?;
+    
+            Ok(Velocity { x, y })
+        }
+    }
+    
+    //` - - - - - struct Robot - - - - -
+
+    /// An abstraction for the robots that guard the bathroom.
+    #[derive(Debug, Clone, Hash)]
+    pub struct Robot {
+        /// The position the robot has in 2D space.
+        position: Point,  
+        /// The velocity the robot has.
+        velocity: Velocity,
+
+    } impl Robot {
+        
+        fn simulate_movement(&mut self) {
+            
+            let mut new_position = self.position + self.velocity;
+
+            if new_position.x < 0 { 
+                new_position.x = BATHROOM_DIMENSIONS.width + new_position.x; 
+            } if new_position.x >= BATHROOM_DIMENSIONS.width  { 
+                new_position.x = -1 * (BATHROOM_DIMENSIONS.width - new_position.x); 
+            } 
+            
+            if new_position.y < 0 { 
+                new_position.y = BATHROOM_DIMENSIONS.height + new_position.y; 
+            } if new_position.y >= BATHROOM_DIMENSIONS.height { 
+                new_position.y = -1 * (BATHROOM_DIMENSIONS.height - new_position.y); 
+            }
+            self.position = new_position;
+        }
+    
+    } impl FromStr for Robot { 
+        type Err = ParseError;
+
+        fn from_str(input_string: &str) -> Result<Self, Self::Err> {
+
+            let (position_string, velocity_string) = input_string.split_once(" ")
+                .ok_or(ParseError::InvalidFormat)?;
+            
+            let position_string = position_string.split_once("=")
+                .ok_or(ParseError::InvalidFormat)?;
+            let position = Point::from_str(position_string.1)?;
+
+            let velocity_string = velocity_string.split_once("=")
+                .ok_or(ParseError::InvalidFormat)?;
+            let velocity = Velocity::from_str(velocity_string.1)?;
+    
+            Ok(Robot { position, velocity })
+        }
+        
+    }
+    
+    //` - - - - - fn solve - - - - -
+
+    /// The function that solves the puzzle.
+    pub fn solve(input: &str) -> usize { 
+
+        let mut robots = Vec::new();
+        for line in input.lines() { 
+            let robot = Robot::from_str(line).unwrap();
+            robots.push(robot);
+        }
+
+        let mut round = 6493 - 25;
+
+        for _ in 0..round  { for robot in robots.iter_mut() { robot.simulate_movement(); } }
+
+        loop {
+            for robot in robots.iter_mut() { robot.simulate_movement(); }
+            println!();
+            println!("Right now it's round {}:", round);
+            debug_bathroom(&robots);
+            println!();
+            print!("Is this a easter-egg being displayed? (y)es / (N)o ");
+            let mut io_in = String::new();
+            io::stdin().read_line(&mut io_in).expect("Failed to read line");
+            let io_in = io_in.to_lowercase();
+            match io_in.as_str() {
+                "y" | "yes" => { return round; },
+                "n" | "no" => { continue; }
+                _ => println!("That is not one of the options")
+            }
+            round += 1;
+        }
+    }
+
+    /// Prints the bathrooms current state
+    fn debug_bathroom(robots: &Vec<Robot>) {
+        let mut bathroom = vec![vec![0; BATHROOM_DIMENSIONS.width as usize]; BATHROOM_DIMENSIONS.height as usize];
+        println!("bathroom = ({}, {})", bathroom[0].len(), bathroom.len());
+        for robot in robots {
+            bathroom[robot.position.y as usize][robot.position.x as usize] += 1;
+        }
+
+        for row in bathroom {
+            for tile in row {
+                if tile == 0 { print!(" ") }
+                else { print!("#")}
+            }
+            println!();
+        }
+    }
 }
 
 
