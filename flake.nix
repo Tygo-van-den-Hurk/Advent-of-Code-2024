e37f7dcd4e874b17f08a124b1e84859718fbd3db
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2024-12-15 13:24:56 +0100

    Made the flake shell hook text DRY.

--- flake.nix
+++ flake.nix
@@ -10,16 +10,8 @@
   outputs = inputs: with inputs; flake-utils.lib.eachDefaultSystem (system:
       let
 
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cargo2nix.overlays.default ];
-        };
-        
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.75.0";
-          packageFun = import ./Cargo.nix;
-        };
-
+        pkgs = import nixpkgs { inherit system; overlays = [ cargo2nix.overlays.default ]; };
+        rustPkgs = pkgs.rustBuilder.makePackageSet { rustVersion = "1.75.0"; packageFun = import ./Cargo.nix; };
         overrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
 
       in rec {
@@ -32,19 +24,37 @@
         devShell = pkgs.mkShell {
           
           RUSTC_VERSION = overrides.toolchain.channel;
-          RUST_BACKTRACE = 1;
+          RUST_BACKTRACE = "full";
 
           buildInputs = with pkgs; [ cargo rustc rustup rust-analyzer pkg-config openssl cargo-watch clippy ];
-          shellHook = (''
-            # if the terminal supports color.
+          
+          shellHook = let 
+
+            shell-hook-text = (can-display-colors: let 
+            
+              black  = ( ''\033[1;30m'' );
+              red    = ( ''\033[1;31m'' );
+              green  = ( ''\033[1;32m'' );
+              yellow = ( ''\033[1;33m'' );
+              blue   = ( ''\033[1;34m'' );
+              purple = ( ''\033[1;35m'' );
+              cyan   = ( ''\033[1;36m'' );
+              white  = ( ''\033[0;37m'' );
+
+              color  = (string: color: if (can-display-colors == true) then "${color}${string}${white}" else string);
+
+            in ''
+              echo -e "Detected ${color system cyan}: started a ${color "Rust" red} development Shell powered by ${color "Nix" blue}."
+              echo -e "Using ${color "$(cargo --version)" yellow} together with ${color "$(rustc --version)" yellow}."
+              export PS1="(${color "Dev-Shell" purple}) $PS1"
+            '');
+
+          in (''
+            # if the terminal supports color. display the message with color, else just use black and white.
             if [[ -n "$(tput colors)" && "$(tput colors)" -gt 2 ]]; then
-              echo -e "\033[1;32mStarted\033[0m a \033[1;31mRust\033[0m Development Shell powered by \033[1;34mNix\033[0m."
-              echo -e "Using \033[1;33m$(cargo --version)\033[00m together with \033[1;33m$(rustc --version)\033[00m."
-              export PS1="(\033[1m\033[35mDev-Shell\033[0m) $PS1"
+              ${shell-hook-text true}
             else 
-              echo "Started a Rust Development Shell powered by Nix."
-              echo "Using $(cargo --version) together with $(rustc --version)."
-              export PS1="(Dev-Shell) $PS1"
+              ${shell-hook-text false}
             fi''
           );
         };
